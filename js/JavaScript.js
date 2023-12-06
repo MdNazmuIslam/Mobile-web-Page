b61e70005f02f70d0c2e5e24359cf3aa8a044780
Md Nazmul Isalm <73328994+MdNazmuIslam@users.noreply.github.com>
2023-12-06 14:10:39 +0600

    Update JavaScript.js

--- js/JavaScript.js
+++ js/JavaScript.js
@@ -78,16 +78,16 @@ progress.oninput = (() => {
     sliderValue.style.left = (inputValue * 1.6) + "%";
 })
 // #range-progress
-// hover effect
+// click effect
 var btnEffect = document.querySelectorAll(".recording-Types .btn-effect");
 btnEffect.forEach((btns)=>{
    
-    btns.addEventListener("mouseover",()=>{
+    btns.addEventListener("click",()=>{
         btns.classList.add("spanColor");
     })
 })
 btnEffect.forEach((btns)=>{
-    btns.addEventListener("mouseout",()=>{
+    btns.addEventListener("click",()=>{
         btns.classList.remove("spanColor");
     })
 })
